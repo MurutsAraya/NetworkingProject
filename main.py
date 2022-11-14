c6f7fd94648f0025a73c3a05e2c4e325a2cec9be
MurutsAraya <mharaya@iu.edu>
2022-11-14 18:59:32 -0500

    Updated Networking project.

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-
-# Import required library
 import turtle
 
-
-# Create screen
+# We are creating here the screen with a green background color, size, and screen tittle name.
 sc = turtle.Screen()
 sc.title("Two players Pong game")
 sc.bgcolor("green")
 sc.setup(width=800, height=400)
 
-# Left paddle
-left_pad = turtle.Turtle()
-left_pad.speed(1)
-left_pad.shape("square")
-left_pad.color("yellow")
-left_pad.shapesize(stretch_wid=12, stretch_len=2)
-left_pad.penup()
-left_pad.goto(-300, 0)
+# we are defining the left paddle shape, color, and shape size.
+left_paddle = turtle.Turtle()
+left_paddle.speed(1)
+left_paddle.shape("square")
+left_paddle.color("yellow")
+left_paddle.shapesize(stretch_wid=12, stretch_len=2)
+left_paddle.penup()
+left_paddle.goto(-300, 0)
 
-# Right paddle
-right_pad = turtle.Turtle()
-right_pad.speed(0)
-right_pad.shape("square")
-right_pad.color("yellow")
-right_pad.shapesize(stretch_wid=12, stretch_len=2)
-right_pad.penup()
-right_pad.goto(400, 0)
+# This is defining the right paddle shape, color, and shape size.
+right_paddle = turtle.Turtle()
+right_paddle.speed(0)
+right_paddle.shape("square")
+right_paddle.color("yellow")
+right_paddle.shapesize(stretch_wid=12, stretch_len=2)
+right_paddle.penup()
+right_paddle.goto(400, 0)
 
-# Ball of circle shape
+# This is showing the ball shape and color.
 hit_ball = turtle.Turtle()
 hit_ball.speed(40)
 hit_ball.shape("circle")
@@ -37,11 +34,11 @@ hit_ball.goto(0, 0)
 hit_ball.dx = 5
 hit_ball.dy = -5
 
-# Initialize the score
+# here we initialize the score for both players.
 Player_A = 0
 Player_B = 0
 
-# Displays the score
+# Outputting the score with blue color
 sketch = turtle.Turtle()
 sketch.speed(0)
 sketch.color("blue")
@@ -52,32 +49,31 @@ sketch.write("Player_A : 0    Player_B: 0",
              align="center", font=("Courier", 24, "normal"))
 
 
-# Functions to move paddle vertically
+# This is the functions to move the paddle vertically or up and down.
 def paddleaup():
-    y = left_pad.ycor()
+    y = left_paddle.ycor()
     y += 20
-    left_pad.sety(y)
+    left_paddle.sety(y)
 
 
 def paddleadown():
-    y = left_pad.ycor()
+    y = left_paddle.ycor()
     y -= 20
-    left_pad.sety(y)
+    left_paddle.sety(y)
 
 
 def paddlebup():
-    y = right_pad.ycor()
+    y = right_paddle.ycor()
     y += 20
-    right_pad.sety(y)
+    right_paddle.sety(y)
 
 
 def paddlebdown():
-    y = right_pad.ycor()
+    y = right_paddle.ycor()
     y -= 20
-    right_pad.sety(y)
+    right_paddle.sety(y)
 
 
-# Keyboard bindings
 sc.listen()
 sc.onkeypress(paddleaup, "e")
 sc.onkeypress(paddleadown, "x")
@@ -90,7 +86,6 @@ while True:
     hit_ball.setx(hit_ball.xcor() + hit_ball.dx)
     hit_ball.sety(hit_ball.ycor() + hit_ball.dy)
 
-    # Checking borders
     if hit_ball.ycor() > 280:
         hit_ball.sety(280)
         hit_ball.dy *= -1
